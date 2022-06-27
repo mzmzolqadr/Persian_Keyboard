89cb2957191e4d9b09bfdb750a7ff221b894f187
Mohammad Zolghadr <mzmzolqadr@gmail.com>
2022-06-27 14:47:08 +0430

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,262 @@
+let currentTyped = [];
+
+function clicker01() {
+  const typedLetter = let01Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+
+let01Key.addEventListener("click", clicker01);
+
+function clicker02() {
+  const typedLetter = let02Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+
+let02Key.addEventListener("click", clicker02);
+
+function clicker03() {
+  const typedLetter = let03Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+
+let03Key.addEventListener("click", clicker03);
+
+function clicker04() {
+  const typedLetter = let04Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let04Key.addEventListener("click", clicker04);
+
+function clicker05() {
+  const typedLetter = let05Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let05Key.addEventListener("click", clicker05);
+
+function clicker06() {
+  const typedLetter = let06Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let06Key.addEventListener("click", clicker06);
+
+function clicker07() {
+  const typedLetter = let07Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let07Key.addEventListener("click", clicker07);
+
+function clicker08() {
+  const typedLetter = let08Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let08Key.addEventListener("click", clicker08);
+
+function clicker09() {
+  const typedLetter = let09Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let09Key.addEventListener("click", clicker09);
+
+function clicker10() {
+  const typedLetter = let10Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let10Key.addEventListener("click", clicker10);
+
+function clicker11() {
+  const typedLetter = let11Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let11Key.addEventListener("click", clicker11);
+
+function clicker12() {
+  const typedLetter = let12Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let12Key.addEventListener("click", clicker12);
+
+function clicker13() {
+  const typedLetter = let13Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let13Key.addEventListener("click", clicker13);
+
+function clicker14() {
+  const typedLetter = let14Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let14Key.addEventListener("click", clicker14);
+
+function clicker15() {
+  const typedLetter = let15Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let15Key.addEventListener("click", clicker15);
+
+function clicker16() {
+  const typedLetter = let16Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let16Key.addEventListener("click", clicker16);
+
+function clicker17() {
+  const typedLetter = let17Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let17Key.addEventListener("click", clicker17);
+
+function clicker18() {
+  const typedLetter = let18Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let18Key.addEventListener("click", clicker18);
+
+function clicker19() {
+  const typedLetter = let19Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let19Key.addEventListener("click", clicker19);
+
+function clicker20() {
+  const typedLetter = let20Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let20Key.addEventListener("click", clicker20);
+
+function clicker21() {
+  const typedLetter = let21Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let21Key.addEventListener("click", clicker21);
+
+function clicker22() {
+  const typedLetter = let22Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let22Key.addEventListener("click", clicker22);
+
+function clicker23() {
+  const typedLetter = let23Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let23Key.addEventListener("click", clicker23);
+
+function clicker24() {
+  const typedLetter = let24Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let24Key.addEventListener("click", clicker24);
+
+function clicker25() {
+  const typedLetter = let04Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let25Key.addEventListener("click", clicker25);
+
+function clicker26() {
+  const typedLetter = let26Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let26Key.addEventListener("click", clicker26);
+
+function clicker27() {
+  const typedLetter = let27Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let27Key.addEventListener("click", clicker27);
+
+function clicker28() {
+  const typedLetter = let28Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let28Key.addEventListener("click", clicker28);
+
+function clicker29() {
+  const typedLetter = let29Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let29Key.addEventListener("click", clicker29);
+
+function clicker30() {
+  const typedLetter = let30Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let30Key.addEventListener("click", clicker30);
+
+function clicker31() {
+  const typedLetter = let31Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let31Key.addEventListener("click", clicker31);
+
+function clicker32() {
+  const typedLetter = let32Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let32Key.addEventListener("click", clicker32);
+
+function clicker33() {
+  const typedLetter = let33Key.innerText;
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+let33Key.addEventListener("click", clicker33);
+
+function space() {
+  const typedLetter = " ";
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+spaceKey.addEventListener("click", space);
+
+function backspace() {
+  currentTyped = currentTyped.slice(0, -1);
+  outputResult(currentTyped);
+}
+backSpace.addEventListener("click", backspace);
+
+function questioner() {
+  const typedLetter = "؟";
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+questionMark.addEventListener("click", questioner);
+
+function dotter() {
+  const typedLetter = ".";
+  currentTyped = currentTyped + typedLetter;
+  outputResult(currentTyped);
+}
+dotMark.addEventListener("click", dotter);
